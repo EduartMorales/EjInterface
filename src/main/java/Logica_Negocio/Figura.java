aecaa678b4a675cfc1951d92579bc6dd392f02d1
1061688186 <1061688186@SJ209PC7.fup.edu>
2024-09-25 17:03:03 -0500

    Eduart Interface

---
+++ src/main/java/Logica_Negocio/Figura.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Logica_Negocio;
+
+/**
+ *
+ * @author 1061688186
+ */
+public interface Figura {
+    public double CalcularArea(); //Aqui defino el que
+    public void DibujarFigura();
+}
